98af7c6abe9f4c89f23142df06494d5d5386952f
sunitsen <69672778+sunitsen@users.noreply.github.com>
2022-05-07 00:57:54 +0600

    message

---
+++ assets/template-js/template.js
@@ -0,0 +1,60 @@
+$(".circle_percent").each(function() {
+    var $this = $(this),
+		$dataV = $this.data("percent"),
+		$dataDeg = $dataV * 3.6,
+		$round = $this.find(".round_per");
+	$round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");	
+	$this.append('<div class="circle_inbox"><span class="percent_text"></span></div>');
+	$this.prop('Counter', 0).animate({Counter: $dataV},
+	{
+		duration: 2000, 
+		easing: 'swing', 
+		step: function (now) {
+            $this.find(".percent_text").text(Math.ceil(now)+"%");
+        }
+    });
+	if($dataV >= 51){
+		$round.css("transform", "rotate(" + 360 + "deg)");
+		setTimeout(function(){
+			$this.addClass("percent_more");
+		},1000);
+		setTimeout(function(){
+			$round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+		},1000);
+	} 
+});
+
+
+//video  popup
+$(document).ready(function () {
+    $(".videoplay").magnificPopup({
+        type: "iframe",
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">' + '<div class="mfp-close"></div>' + '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' + "</div>", // HTML markup of popup, `mfp-close` will be replaced by the close button
+  
+            patterns: {
+                youtube: {
+                    index: "youtube.com/", 
+  
+                    id: "v=", 
+  
+                    src: "https://www.youtube.com/embed/%id%?autoplay=1",
+                },
+                vimeo: {
+                    index: "vimeo.com/",
+                    id: "/",
+                    src: "//player.vimeo.com/video/%id%?autoplay=1",
+                },
+                gmaps: {
+                    index: "//maps.google.",
+                    src: "%id%&output=embed",
+                },
+  
+            },
+  
+            srcAction: "iframe_src",
+        },
+    });
+  });
+  
+  
